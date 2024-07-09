758adf0dce6144c651ee7381bd562706ecab64af
ozge belen <ozgebelen@ozge-MacBook-Pro.local>
2024-07-09 16:24:56 +0300

    startGameScreen için confirm durumunda eğer istenilen bilgi gelmezse alert gösterilmesi ve resethandler yapıldı

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet , Alert} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import {useState} from 'react'
 
@@ -9,8 +9,23 @@ const StartGameScreen = () => {
         setEnteredNumber(enteredText)
 
     }
-    const confirmInputHandler = ()=> {
 
+    const resetInputHandler = ()=> {
+        setEnteredNumber('');
+    }
+    const confirmInputHandler = ()=> {
+        const chosenNumber = parseInt(enteredNumber);
+
+        if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber >99){
+
+            Alert.alert(
+            'Invalid number',
+            'Number has to be a number between 1 and 99.',
+            [{text: 'Okay', style: 'destructive', onPress:resetInputHandler}]
+            );
+            return;
+        } 
+            console.log('valid number')
     }
 
   return (
@@ -27,7 +42,7 @@ const StartGameScreen = () => {
 
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
           <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
